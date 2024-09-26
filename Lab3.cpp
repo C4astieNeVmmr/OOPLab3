870e372a15d63ad393453c3a03a57a4ef4c65d35
C4astieNeVmmr <nevmmr@gmail.com>
2024-09-26 19:06:18 +0700

    first commit

---
+++ Lab3.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+template <class T>
+class TVector{
+    T* arr;
+    int len;
+    public:
+    TVector(int l=1){
+        this->len = l;
+        this->arr = new T[this->len];
+    }
+    TVector(const TVector<T>& vectorToCopy){
+        this->len = vectorToCopy.len;
+        this->arr = new T[this->len];
+        for(int i=0;i<this->len;i++){
+            this->arr[i] = vectorToCopy.arr[i];
+        }
+    }
+    ~TVector(){
+        delete[] this->arr;
+    }
+    void setLength(int newLen){
+        T* newArr = new T[newLen];
+        for(int i=0;(i<newLen)&&(i<this->len);i++){
+            newArr[i] = this->arr[i];
+        }
+        this->len = newLen;
+        T* tempPtr=this->arr;
+        this->arr = newArr;
+        delete[] tempPtr;
+    }
+    T& operator[](int index){
+        if(index>=(this->len)){
+            throw std::out_of_range("");
+        }
+        return this->arr[index];
+    }
+    /*TVector<T> operator=(TVector<T> vectorB){
+
+    }
+    TVector<T> operator+(TVector<T> vectorB){
+        
+    }
+    TVector<T> operator+=(TVector<T> vectorB){
+        if(this->len!=vectorB.len){
+            throw out_of_range("");
+        }
+        for(int i=0;i<this->len;i++){
+            this->arr[i] += vectorB.arr[i];
+        }
+    }*/
+    friend std::ostream& operator<<(std::ostream& os,TVector<T> vec){
+        for(int i=0;i<vec.len;i++){
+            os << vec[i] << " ";
+        }
+        return os;
+    }
+};
+
+int main(){
+    int a = 10, b = 15;
+    TVector<char> charVector(a);
+    TVector<int> intVectorA(a),intVectorB(b);
+    for(int i=0;i<a;i++){
+        charVector[i] = 'A'+i;
+        intVectorA[i] = 2*i;
+    }
+    for(int i=0;i<b;i++){
+        intVectorB[i] = 2*i+1;
+    }
+    std::cout << "charVector = " << charVector;
+    std::cout << "\n\n intVectorA = " << intVectorA;
+    std::cout << "\n\n intVectorB = " << intVectorB << "\n\n\n";
+    intVectorA[0] = 314;
+    intVectorB[0] = 271;
+    std::cout << "\n\n intVectorA = " << intVectorA;
+    std::cout << "\n\n intVectorB = " << intVectorB << "\n\n\n";
+    intVectorA.setLength(20);
+    intVectorB.setLength(5);
+    std::cout << "\n\n intVectorA = " << intVectorA;
+    std::cout << "\n\n intVectorB = " << intVectorB << "\n\n\n";
+    //std::cout << intVectorB[10]=5; out_of_range
+    return 0;
+}
